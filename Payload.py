ba44a1c95617e73c3e3f849c70aa368e07a179a5
anjanadw <anjana@digitalwaters.org>
2024-09-06 19:33:34 -0400

    Update Payload.py

--- Payload.py
+++ Payload.py
@@ -3,8 +3,9 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 import os
 from datetime import datetime, timezone
 
-def uploadPayload(payloadData, log):
-    url = "https://water-watch-58265eebffd9.herokuapp.com/upload/"
+def uploadPayload(payloadData, log, secrets):
+    url = secrets["apiURL"]
+    apiKey = secrets["apiKey"]
     boundary = "*****"
 
     # Get the device ID from the payload data
@@ -12,7 +13,7 @@ def uploadPayload(payloadData, log):
 
     latitude = str(payloadData.get('latitude', "999"))
     longitude = str(payloadData.get('longitude', "999"))
-    dateTime = payloadData.get('dateTime', datetime.now().strftime("%Y-%m-%d"))
+    dateTime = payloadData.get('dateTime', datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"))
     
     # Preparing the data to be sent
     fields = {
@@ -24,36 +25,27 @@ def uploadPayload(payloadData, log):
     }
 
     currDirectory = os.path.dirname(os.path.abspath(__file__))
-    if "image" in payloadData:
-     filePath = os.path.join(currDirectory, payloadData["image"])
-    else:
-     log.error("No image key found in payloadData")
-     return
+    filePath = os.path.join(currDirectory, payloadData["image"])
     # Adding the image file to the fields
-    try:
-     with open(filePath, 'rb') as file:
-         fields['image'] = (os.path.basename(filePath), file, 'image/jpeg')
-    except FileNotFoundError:
-     log.error(f"Image file not found: {filePath}")
-    return
-    
+    with open(filePath, 'rb') as file:
+        fields['image'] = (os.path.basename(filePath), file, 'image/jpeg')
         
-    # Creating a MultipartEncoder
-    m = MultipartEncoder(fields=fields, boundary=boundary)
+        # Creating a MultipartEncoder
+        m = MultipartEncoder(fields=fields, boundary=boundary)
 
-    # Debugging information
-    log.info(f"Uploading file: {filePath}")
-    log.info(f"Data being sent: {fields}")
+        # Debugging information
+        log.info(f"Uploading file: {filePath}")
+        log.info(f"Data being sent: {fields}")
 
-    # Sending the request
-    try:
-        response = requests.post(url, data=m, headers={'Content-Type': m.content_type})
-        if response.status_code == 200:
-            log.info("Successfully uploaded payload to DB")
-        else:
+        # Sending the request
+        try:
+            response = requests.post(url, data=m, headers={'Content-Type': m.content_type})
+            if response.status_code == 200:
+                log.info("Successfully uploaded payload to DB")
+            else:
                 log.error(f"Upload Failed: {response.status_code} - {response.reason}")
                 log.error(f"Response Text: {response.text}")
                 log.error(f"Response Headers: {response.headers}")
                 response.raise_for_status()  # Raise an error for bad status codes
-    except Exception as e:
-        log.error(f"Upload Exception: {e}")
+        except Exception as e:
+            log.error(f"Upload Exception: {e}")
